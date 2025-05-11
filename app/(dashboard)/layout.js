c3e5facbcb6f1cf298348224e8a57ddc8ce4fc31
Kanon-Hosen <kanon5866@gmail.com>
2025-05-12 01:51:32 +0600

    Navbar refactor

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+export const metadata = {
+  title: "Dashboard",
+  description:
+    "Tenord is a simple and fast web application for buying and selling flats in Dhaka for rent.",
+};
+
+export default function RootDashboardLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
