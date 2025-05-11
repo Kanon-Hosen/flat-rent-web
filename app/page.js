c232cc31ef2430288af7f9495f3d70bc511af477
Nameless146 <mrnameless44@gmail.com>
2025-05-11 21:16:08 +0600

    created the navbar component

--- app/page.js
+++ app/page.js
@@ -1,8 +1,39 @@
-import { Navbar } from "@/components/Navbar";
+import  Navbar  from "@/components/Navbar";
+import Head from "next/head";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
+
+
+      <Head>
+
+
+
+  <title>Tenord</title>
+
+  <meta name="description" content="Tenord is a simple and fast web application for buying and selling flats in Dhaka for rent." />
+
+  {/* Correct way to set the favicon */}
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+
+</Head>
+
+    
+
+
+
+
       <Navbar />
+
+
+
+      
+
+
+
     </>
   );
 }
